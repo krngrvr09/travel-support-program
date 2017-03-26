c21e6c65626051c511f9f2ca5c739e888cc73449
krngrvr09 <krngrvr09@gmail.com>
2017-03-26 17:37:23 +0530

    added test test

--- spec/support/common_helpers.rb
+++ spec/support/common_helpers.rb
@@ -45,11 +45,36 @@ module CommonHelpers
     puts request
     puts request.event.name
     puts current_url
+
     sign_in_as_user(user, opts)
     visit travel_sponsorships_path
     # Use the event filter
     show_bootstrap_multiselect_select("#q_event_id_in")
     select(request.event.name, :from => "q_event_id_in")
+    click_button "search"
+    # Check the url to ensure that the form have been submitted
+    current_url.should match /event_id_in/
+    # If so, the request should be in the first page
+    find(:xpath, "//table[contains(@class,'requests')]//tbody/tr/td[1]//a[text()='##{request.id}']").click
+    page.should have_content "request"
+    request.expenses.each do |e|
+      page.should have_content e.subject
+      page.should have_content e.description
+    end
+  end
+
+  def find_request_by_search(user, request, opts = {})
+    puts "!@!@!@!!@!@!@!@!@!@!@!@!@!@!@!@!@!@!@!@!"
+    puts request
+    puts request.event.name
+    puts current_url
+    
+    sign_in_as_user(user, opts)
+    visit travel_sponsorships_path
+    # Use the event filter
+    fill_in 'q_user_nickname_or_event_name_or_user_profile_full_name_cont', :with => 'tspmember'
+    # show_bootstrap_multiselect_select("#q_event_id_in")
+    # select(request.event.name, :from => "q_event_id_in")
     click_button "search"
     # Check the url to ensure that the form have been submitted
     current_url.should match /event_id_in/
