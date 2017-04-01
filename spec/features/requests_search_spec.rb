d97c38a19b66c3a65ccb3a06c102ce66b87a7991
krngrvr09 <krngrvr09@gmail.com>
2017-04-02 01:02:53 +0530

    added request creation in all scenarios

--- spec/features/requests_search_spec.rb
+++ spec/features/requests_search_spec.rb
@@ -2,32 +2,33 @@ require 'spec_helper'
 
 feature "Requests Search", "" do
 	fixtures :all
-	sign_in_as_user(users(:luke))
-	visit event_path(events(:dagobah_camp))
-	click_link "Travel support"
-
-	# Request creation
-	page.should have_content "New travel support request"
-	fill_in "travel_sponsorship_description", :with => "I need to go because a ghost told me to do it"
-	select "Gas", :from => "travel_sponsorship_expenses_attributes_0_subject"
-	fill_in "travel_sponsorship_expenses_attributes_0_description", :with => "Gas"
-	fill_in "travel_sponsorship_expenses_attributes_0_estimated_amount", :with => "100"
-	select "EUR", :from => "travel_sponsorship_expenses_attributes_0_estimated_currency"
-	click_link "add expense"
-	within(:xpath, "//tr[@class='nested-fields'][last()]") do
-	  find('select[name$="[subject]"]').set "Droid rental"
-	  find('input[name$="[description]"]').set "R2D2"
-	  #find('input[name$="[estimated_amount]"]').set "50"
-	  #find('input[name$="[estimated_currency]"]').set "EUR"
-	end
-	click_button "Create travel support request"
-	page.should have_content "request was successfully created"
-	page.should have_content "then submit the request using the 'Action' button"
-	request = Request.order(:created_at, :id).last
-	click_link "Log out"
 	scenario "Search by full name", :js => true do
-		opts={}
+		sign_in_as_user(users(:luke))
+		visit event_path(events(:dagobah_camp))
+		click_link "Travel support"
+
+		# Request creation
+		page.should have_content "New travel support request"
+		fill_in "travel_sponsorship_description", :with => "I need to go because a ghost told me to do it"
+		select "Gas", :from => "travel_sponsorship_expenses_attributes_0_subject"
+		fill_in "travel_sponsorship_expenses_attributes_0_description", :with => "Gas"
+		fill_in "travel_sponsorship_expenses_attributes_0_estimated_amount", :with => "100"
+		select "EUR", :from => "travel_sponsorship_expenses_attributes_0_estimated_currency"
+		click_link "add expense"
+		within(:xpath, "//tr[@class='nested-fields'][last()]") do
+		  find('select[name$="[subject]"]').set "Droid rental"
+		  find('input[name$="[description]"]').set "R2D2"
+		  #find('input[name$="[estimated_amount]"]').set "50"
+		  #find('input[name$="[estimated_currency]"]').set "EUR"
+		end
+		click_button "Create travel support request"
+		page.should have_content "request was successfully created"
+		page.should have_content "then submit the request using the 'Action' button"
+		request = Request.order(:created_at, :id).last
+		click_link "Log out"
+
 		# Search request using full name
+		opts={}
 		sign_in_as_user(users(:tspmember), opts)
 		visit travel_sponsorships_path
 		# Use the event filter
@@ -46,6 +47,29 @@ feature "Requests Search", "" do
 	end
 
 	scenario "Search by nickname", :js => true do
+		sign_in_as_user(users(:luke))
+		visit event_path(events(:dagobah_camp))
+		click_link "Travel support"
+
+		# Request creation
+		page.should have_content "New travel support request"
+		fill_in "travel_sponsorship_description", :with => "I need to go because a ghost told me to do it"
+		select "Gas", :from => "travel_sponsorship_expenses_attributes_0_subject"
+		fill_in "travel_sponsorship_expenses_attributes_0_description", :with => "Gas"
+		fill_in "travel_sponsorship_expenses_attributes_0_estimated_amount", :with => "100"
+		select "EUR", :from => "travel_sponsorship_expenses_attributes_0_estimated_currency"
+		click_link "add expense"
+		within(:xpath, "//tr[@class='nested-fields'][last()]") do
+		  find('select[name$="[subject]"]').set "Droid rental"
+		  find('input[name$="[description]"]').set "R2D2"
+		  #find('input[name$="[estimated_amount]"]').set "50"
+		  #find('input[name$="[estimated_currency]"]').set "EUR"
+		end
+		click_button "Create travel support request"
+		page.should have_content "request was successfully created"
+		page.should have_content "then submit the request using the 'Action' button"
+		request = Request.order(:created_at, :id).last
+		click_link "Log out"
 
 		# Search request using nickname
 		opts={}
@@ -67,6 +91,29 @@ feature "Requests Search", "" do
 	end
 
 	scenario "Search by event name", :js => true do
+		sign_in_as_user(users(:luke))
+		visit event_path(events(:dagobah_camp))
+		click_link "Travel support"
+
+		# Request creation
+		page.should have_content "New travel support request"
+		fill_in "travel_sponsorship_description", :with => "I need to go because a ghost told me to do it"
+		select "Gas", :from => "travel_sponsorship_expenses_attributes_0_subject"
+		fill_in "travel_sponsorship_expenses_attributes_0_description", :with => "Gas"
+		fill_in "travel_sponsorship_expenses_attributes_0_estimated_amount", :with => "100"
+		select "EUR", :from => "travel_sponsorship_expenses_attributes_0_estimated_currency"
+		click_link "add expense"
+		within(:xpath, "//tr[@class='nested-fields'][last()]") do
+		  find('select[name$="[subject]"]').set "Droid rental"
+		  find('input[name$="[description]"]').set "R2D2"
+		  #find('input[name$="[estimated_amount]"]').set "50"
+		  #find('input[name$="[estimated_currency]"]').set "EUR"
+		end
+		click_button "Create travel support request"
+		page.should have_content "request was successfully created"
+		page.should have_content "then submit the request using the 'Action' button"
+		request = Request.order(:created_at, :id).last
+		click_link "Log out"
 		# Search request using event name
 		opts={}
 		sign_in_as_user(users(:tspmember), opts)
